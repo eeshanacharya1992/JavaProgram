cc3438e2354a6b682007c28b0aafd3bcde943142
Eeshan1992 <eeshanacharya@gmail.com>
2024-07-17 15:42:29 +0530

    Java code

---
+++ IfElseIf2345.java
@@ -0,0 +1,25 @@
+package conditional2;
+
+import java.util.Scanner;
+
+public class IfElseIf2345 {
+    static int a,b;
+	public static void main(String[] args) {
+		Scanner s1= new Scanner(System.in);
+	//	int a=s1.nextInt();
+ 	  //  int b=s1.nextInt();
+ 	    if(a>b)
+ 	    {
+ 	       System.out.println("Mechanical Engineering");
+ 	    }
+ 	    else if(b>a)
+ 	    {
+ 	       System.out.println("Automobile Engineering");
+ 	    }
+ 	   else
+	    {
+	    	System.out.println("Mechatronics Engineering");
+	    }
+	}
+
+}
---
+++ IfElseif.java
@@ -0,0 +1,52 @@
+package conditional2;
+public class IfElseif 
+{
+     public static void main(String[] args) 
+     {
+    	int a=100;
+ 	    int b=200;
+ 	    if(a>b)
+ 	    {
+ 	       System.out.println("Mechanical Engineering");
+ 	    }
+ 	    else if(a>b)
+ 	    {
+ 	       System.out.println("Automobile Engineering");
+ 	    }
+ 	    else if(a<=b)
+ 	    {
+ 	    	System.out.println("Chemical Engineering");
+ 	    }
+ 	    else if(a>=b)
+ 	    {
+ 	    	System.out.println("Civil Engineering");
+ 	    }
+ 	    else if(a==b)
+ 	    {
+ 	    	System.out.println("Electrical Engineering");
+ 	    }
+ 	    else if(a!=b)
+ 	    {
+ 	    	System.out.println("Computer Science Engineering");
+ 	    }
+ 	    else if(a>b)
+ 	    {
+ 	    	System.out.println("Mining Engineering");
+ 	    }
+ 	    else if(a>=b)
+ 	    {
+ 	    	System.out.println("Production Engineering");
+ 	    }
+ 	    else if(a<=b)
+ 	    {
+ 	    	System.out.println("Electronics And Communications Engineering");
+ 	    }
+ 	    else
+ 	    {
+ 	    	System.out.println("Mechatronics Engineering");
+ 	    }
+ 	 }
+
+ }
+
+	
